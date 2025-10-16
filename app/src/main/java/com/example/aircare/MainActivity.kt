f1e830902258909b189f51729d6dfc94f06e106a
SebastianObert <obert2878@gmail.com>
2025-10-16 17:02:21 +0700

    Setup View Binding for Home Screen

--- app/src/main/java/com/example/aircare/MainActivity.kt
+++ app/src/main/java/com/example/aircare/MainActivity.kt
@@ -6,16 +6,13 @@ import com.example.aircare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // 1. Deklarasikan variabel binding
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 2. Inisialisasi binding
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        // 3. Set content view menggunakan root dari binding
         setContentView(binding.root)
 
         binding.tvLocation.text = "Tangerang, Indonesia"
